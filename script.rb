61a56ed224c2f1977fd13bc425801a5ce33f97f6
Adrian Jones <adrianjones@duck.com>
2025-03-01 10:40:38 -0600

    fix: broke with a tie
    In the old version of this program, the program would provide incorrect output when there was a tie (i.e. two sets of two days are both equally good days to buy and sell a stock). This old version of the program would output a relatively long array in those cases (e.g. stock_picker([3,1,31,3]) would output [1,3,0,2,4,1,3,0,2,4,1,3,0,2,4]. This updated version of the program uses hashes to fix that, so that only a single pair of days is given (one day to buy, another to sell).

--- script.rb
+++ script.rb
@@ -1,25 +1,21 @@
+require 'pry-byebug'
+
 def stock_picker(days)
   options = []
+  highest_earning = []
   days.each_with_index do |day1, idx1|
     days.each_with_index do |day2, idx2|
       if (idx1 < idx2)
-        options.push([day1, day2, day2-day1])
+        options.push([day2-day1, [idx1, idx2]])
+        highest_earning.push(day2-day1)
       end
     end
   end
 
-  options2 = []
-  options.each { |option| options2.push(option[2]) }
-
-  find_them = []
-  options.each do |arr|
-    if arr[2] == options2.max
-      days.each_with_index { |day, idx| find_them.push(idx) if day == arr[0] }
-      days.each_with_index { |day, idx| find_them.push(idx) if day == arr[1] }
-    end
-  end
-
-  find_them
+  options.to_h[highest_earning.max]
 end
 
 p stock_picker([17,3,6,9,15,8,6,1,10]) #[1,4]
+p stock_picker([2, 4, 1, 8, 3, 9, 2]) #[2,5]
+p stock_picker([3, 1, 3, 1, 3]) #[1,2] or [3,4]
+p stock_picker([14, 6, 1, 19, 15, 8, 6, 1, 20]) #[2,8] or [7,8]
