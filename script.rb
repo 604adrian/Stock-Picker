b5f354b29a683849ccae9b13b9cdde95a1bb3d59
Adrian Jones <adrianjones@duck.com>
2025-03-01 10:53:54 -0600

    Fix: buy and sell on same day
    Before, if there was an array of ascending values, then the method would advise you to make a move that loses money.

--- script.rb
+++ script.rb
@@ -12,10 +12,17 @@ def stock_picker(days)
     end
   end
 
-  options.to_h[highest_earning.max]
+  # conditional to make sure money is never lost
+  if highest_earning.max < 0
+    lowest_cost_day = days.find_index(days.min)
+    return [lowest_cost_day, lowest_cost_day]
+  else
+    options.to_h[highest_earning.max]
+  end
 end
 
 p stock_picker([17,3,6,9,15,8,6,1,10]) #[1,4]
 p stock_picker([2, 4, 1, 8, 3, 9, 2]) #[2,5]
 p stock_picker([3, 1, 3, 1, 3]) #[1,2] or [3,4]
 p stock_picker([14, 6, 1, 19, 15, 8, 6, 1, 20]) #[2,8] or [7,8]
+p stock_picker([9, 8, 7, 6, 5, 4, 3, 2, 1]) #[8,8]
